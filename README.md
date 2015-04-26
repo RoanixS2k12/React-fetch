1656c8f7042c06d61142d51cfce4d301c0e25cb1
Joani COUMA <joani.couma@gmail.com>
2015-04-26 14:16:39 +0200

    move config to master

---
+++ README.md
@@ -0,0 +1,66 @@
+## **React fetch** ##
+
+
+This is a simple repository to try and play with React and some other tools.
+
+
+## Installation ##
+First clone it into a local repo
+
+    git clone https://github.com/RoanixS2k12/React-fetch reactapp
+
+then simple install and build process using `npm` and `gulp`
+
+    cd reactapp
+    npm install
+    gulp
+
+This will launch all the required tools to compile react jsx files & sass files and you will be ready to start coding.
+
+A webserver is active (thanks to `gulp-connect`) on `http://localhost:8081`
+
+## Structure ##
+	
+
+    /app 
+	    /components
+	    App.jsx
+	    main.jsx
+    /build
+	    app.js
+    /assets
+	    /css
+		    main.css
+	    /images
+		    favicon.ico
+    /sass
+    index.html
+    
+
+> Some of this directories will only be visible once you build the application for the first time.
+
+## Tools used ##
+
+ - [gulp](https://github.com/gulpjs/gulp)
+ - [gulp-util](https://github.com/gulpjs/gulp-util)
+ - [gulp-concat](https://github.com/wearefractal/gulp-concat)
+ - [gulp-notify](https://github.com/mikaelbr/gulp-notify)
+ - [gulp-rename](https://github.com/hparra/gulp-rename)
+ - [gulp-connect](https://github.com/hparra/gulp-rename)
+ - [gulp-connect](https://github.com/AveVlad/gulp-connect)
+ - [gulp-sass](https://github.com/dlmanning/gulp-sass)
+ - [browserify](https://github.com/substack/node-browserify)
+ - [reactify](https://github.com/andreypopp/reactify)
+ - [watchify](https://github.com/substack/watchify)
+ - [vinyl-source-stream](https://github.com/hughsk/vinyl-source-stream)
+ - [node-bourbon](https://github.com/lacroixdesign/node-bourbon)
+
+## Libraries ##
+
+  
+
+ -  [react](https://github.com/facebook/react)
+ - [react-favicon](https://github.com/cezary/react-favicon)
+ - [es6-promise](https://github.com/jakearchibald/es6-promise)
+ - [isomorphic-fetch](https://github.com/matthew-andrews/isomorphic-fetch)
+   
--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,69 @@
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
     source = require('vinyl-source-stream'),
     browserify = require('browserify'),
     watchify = require('watchify'),
     reactify = require('reactify'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    connect = require('gulp-connect'),
+    sass = require('gulp-sass'),
+    bourbon = require('node-bourbon');
 
 gulp.task('browserify', function() {
+
     var bundler = browserify({
         entries: ['./app/main.jsx'], // Only need initial file, browserify finds the deps
         transform: [reactify], // We want to convert JSX to normal javascript
         debug: true, // Gives us sourcemapping
         cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
     });
+
     var watcher  = watchify(bundler);
 
-    return watcher
-    .on('update', function () {
-        // var updateStart = Date.now();
-        // console.log('Updating!');
-        watcher.bundle()
-        .pipe(source('main.jsx'))
-        .pipe(rename("app.js"))
-        .pipe(gulp.dest('./build/'))
-        .pipe(notify('Compilation succeeded'));
-        // console.log('Updated!', (Date.now() - updateStart) + 'ms');
-    })
-    .bundle() // Create the initial bundle when starting the task
-    .pipe(source('main.jsx'))
-    .pipe(rename("app.js"))
-    .pipe(gulp.dest('./build/'));
+    watcher.on('update', rebundle);
+    watcher.on('log', gutil.log.bind(gutil));
+
+    function rebundle() {
+        return watcher.bundle()
+            .on('error', gutil.log.bind(gutil, 'JSX Error'))
+            .pipe(source('main.jsx'))
+            .pipe(rename("app.js"))
+            .pipe(gulp.dest('./build/'))
+            .pipe(connect.reload())
+            .pipe(notify('Compilation succeeded'));
+    }
+
+    return rebundle();
+
+});
+
+/* Compiling sass files */
+gulp.task('sass', function() {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass({
+            includePaths: bourbon.includePaths
+        }))
+        .on('error', gutil.log.bind(gutil, 'Sass Error'))
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(connect.reload())
+});
+
+/* Watching for sass file changing */
+gulp.task('watch-sass', function () {
+  return gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+
+/* WEB SERVER */
+gulp.task('connect', function() {
+    connect.server({
+        port : 8081,
+        fallback: 'index.html',
+        livereload: true
+    });
 });
 
 // Just running the two tasks
-gulp.task('default', ['browserify']);
+gulp.task('default', ['connect', 'browserify', 'watch-sass']);
